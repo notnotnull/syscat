1e2d5c09f89548aa7b0385037bbc85567a516b4d
notnotnull <jesteralex@hotmail.com>
2026-03-05 14:01:45 -0500

    add clang-format file and debian package make target

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
 TARGET = syscat
+VERSION = 0.1.0
+
+# Packaging
+DEBIAN_PACKAGE = $(TARGET)_$(VERSION)_amd64.deb
+DEBIAN_CONTROL = dist/debian/DEBIAN/control
+RPM_PACKAGE = $(TARGET)-$(VERSION).x86_64.rpm
+ARCH_PACKAGE = $(TARGET)-$(VERSION)-x86_64.tar.gz
+PACKAGES = $(DEBIAN_PACKAGE) $(RPM_PACKAGE) $(ARCH_PACKAGE)
 SRCS = src/main.c
 BIN_DIR = bin
 CC=gcc
+LINT=clang-format
 RM =rm -f
+VALGRIND_FLAGS = --leak-check=full --track-origins=yes --show-leak-kinds=all
 
 # Compiler and linker flags
 # -Wall: Enable all warnings
@@ -12,12 +22,40 @@ RM =rm -f
 CFLAGS = -Wall -Wextra
 LINK_FLAGS = -nostdlib
 
-.PHONY: all clean
+.PHONY: all clean check lint valgrind dist debian redhat arch
 
 all: $(TARGET)
 
 $(TARGET): $(SRCS)
 	$(CC) $(CFLAGS) -o $(BIN_DIR)/$@ $^ $(LINK_FLAGS)
+
+dist: debian redhat arch
+
+debian: $(TARGET)
+	@echo "Creating Debian package..."
+	mkdir -p dist/debian/usr/bin
+	mkdir -p dist/debian/DEBIAN
+	cp $(BIN_DIR)/$(TARGET) dist/debian/usr/bin/
+	echo "Package: $(TARGET)" > $(DEBIAN_CONTROL)
+	echo "Version: $(VERSION)" >> $(DEBIAN_CONTROL)
+	echo "Priority: optional" >> $(DEBIAN_CONTROL)
+	echo "Architecture: amd64" >> $(DEBIAN_CONTROL)
+	echo "Maintainer: Alex Campbell alex.m.campbell@hotmail.com" >> $(DEBIAN_CONTROL)
+	echo "Description: Lightweight file concatenation tool." >> $(DEBIAN_CONTROL)
+	dpkg-deb --build dist/debian $(DEBIAN_PACKAGE)
+	@echo "Debian package created: $(DEBIAN_PACKAGE)"
+
+check: lint	valgrind
+	@echo "All checks passed!"
+
+lint:
+	$(LINT) -i $(SRCS)
+
+valgrind: $(TARGET)
+	valgrind $(VALGRIND_FLAGS) $(BIN_DIR)/$(TARGET) .gitignore 1>/dev/null
+
+distclean:
+	$(RM) -rf dist $(PACKAGES)
 
 clean:
 	$(RM) $(BIN_DIR)/$(TARGET)
--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
-#define __NR_read  0
+#define __NR_read 0
 #define __NR_write 1
-#define __NR_open  2
+#define __NR_open 2
 #define __NR_close 3
-#define __NR_exit  60
+#define __NR_exit 60
 
 #define O_RDONLY 00
 
@@ -13,146 +13,142 @@
  * https://stackoverflow.com/questions/50260855/how-get-arguments-value-using-inline-assembly-in-c-without-glibc
  */
 #ifdef __x86_64__
-asm (
-    ".global _start\n"
-        "_start:\n"
-        "   xorl %ebp,%ebp\n"       // mark outermost stack frame
-        "   movq 0(%rsp),%rdi\n"    // get argc
-        "   lea 8(%rsp),%rsi\n"     // the arguments are pushed just below, so argv = %rbp + 8
-        "   call main\n"            // call our main
-        "   movq %rax,%rdi\n"       // take the main return code and use it as first argument for...
-        "   movl $60,%eax\n"        // ... the exit syscall
-        "   syscall\n"
-        "   int3\n");               // just in case
+asm (".global _start\n"
+     "_start:\n"
+     "   xorl %ebp,%ebp\n"    // mark outermost stack frame
+     "   movq 0(%rsp),%rdi\n" // get argc
+     "   lea 8(%rsp),%rsi\n"  // the arguments are pushed just below, so argv =
+                              // %rbp + 8
+     "   call main\n"         // call our main
+     "   movq %rax,%rdi\n"    // take the main return code and use it as first
+                              // argument for...
+     "   movl $60,%eax\n"     // ... the exit syscall
+     "   syscall\n"
+     "   int3\n"); // just in case
 #endif
 
 /**
  * GCC Attributes, Modifiers, Quantifiers Used:
- * 
+ *
  * always_inline - forces inlining of functions when not optimizing.
  * inline - direct GCC to make calls to this function faster.
  */
 
-
 /**
  * @brief Execute a syscall with 1 argument.
- * 
+ *
  * @param n - syscall number.
  * @param a1 - argument 1.
- * 
+ *
  * @return long - syscall's return value
  */
-static inline __attribute__((always_inline))
-long syscall1(long n, long a1) {
-    long ret;
-    asm (
-        "syscall\n\t"
-        : "=a"(ret)
-        : "a"(n), "D"(a1)
-        : "memory", "rcx"
-    );
-    return ret;
+static inline __attribute__ ((always_inline)) long
+syscall1 (long n, long a1)
+{
+  long ret;
+  asm ("syscall\n\t" : "=a"(ret) : "a"(n), "D"(a1) : "memory", "rcx");
+  return ret;
 }
 
 /**
  * @brief Execute a syscall with 2 arguments.
- * 
+ *
  * @param n - syscall number.
  * @param a1 - argument 1.
  * @param a2 - argument 2.
- * 
+ *
  * @return long - syscall's return value
  */
-static inline __attribute__((always_inline))
-long syscall2(long n, long a1, long a2) {
-    long ret;
-    asm (
-        "syscall\n\t"
-        : "=a"(ret)
-        : "a"(n), "D"(a1), "S"(a2)
-        : "memory", "rcx"
-    );
-    return ret;
+static inline __attribute__ ((always_inline)) long
+syscall2 (long n, long a1, long a2)
+{
+  long ret;
+  asm ("syscall\n\t" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2) : "memory", "rcx");
+  return ret;
 }
 
 /**
  * @brief Execute a syscall with 3 arguments.
- * 
+ *
  * @param n - syscall number.
  * @param a1 - argument 1.
  * @param a2 - argument 2.
  * @param a3 - argument 3.
- * 
+ *
  * @return long - syscall's return value
  */
-static inline __attribute__((always_inline))
-long syscall3(long n, long a1, long a2, long a3) {
-    long ret;
-    asm (
-        "syscall\n\t"
-        : "=a"(ret)
-        : "a"(n), "D"(a1), "S"(a2), "d"(a3)
-        : "memory", "rcx"
-    );
-    return ret;
+static inline __attribute__ ((always_inline)) long
+syscall3 (long n, long a1, long a2, long a3)
+{
+  long ret;
+  asm ("syscall\n\t"
+       : "=a"(ret)
+       : "a"(n), "D"(a1), "S"(a2), "d"(a3)
+       : "memory", "rcx");
+  return ret;
 }
 
 /**
  * @brief Execute a syscall with 4 arguments.
- * 
+ *
  * @param n - syscall number.
  * @param a1 - argument 1.
  * @param a2 - argument 2.
  * @param a3 - argument 3.
  * @param a3 - argument 4.
- * 
+ *
  * @return long - syscall's return value
  */
-static inline __attribute__((always_inline))
-long syscall4(long n, long a1, long a2, long a3, long a4) {
-    long ret;
-    register long r10 __asm__("r10") = a4;
-    asm (
-        "syscall\n\t"
-        : "=a"(ret)
-        : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10)
-        : "memory", "rcx"
-    );
-    return ret;
+static inline __attribute__ ((always_inline)) long
+syscall4 (long n, long a1, long a2, long a3, long a4)
+{
+  long ret;
+  register long r10 __asm__ ("r10") = a4;
+  asm ("syscall\n\t"
+       : "=a"(ret)
+       : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10)
+       : "memory", "rcx");
+  return ret;
 }
 
 /**
  * @brief Reset the buffer.
- * 
+ *
  * @param buffer (char *): address of the buffer.
- * 
+ *
  */
-static inline __attribute__ ((always_inline))
-void clear_buff(char *buffer) {
-    for (int i = 0; i < BUFFER_SIZE; ++i) {
-        buffer[i] = 0;
+static inline __attribute__ ((always_inline)) void
+clear_buff (char *buffer)
+{
+  for (int i = 0; i < BUFFER_SIZE; ++i)
+    {
+      buffer[i] = 0;
     }
 }
 
-int main(int argc, char *argv[]) {
-    // open file
-    int fd = 0;
-    fd = syscall2(__NR_open, (long)argv[1], O_RDONLY);
-    if (0 > fd) {
-        syscall3(__NR_write, 2, (long)"Error opening file\n", 19);
-        syscall1(__NR_exit, 1);
+int
+main (int argc, char *argv[])
+{
+  // open file
+  int fd = 0;
+  fd = syscall2 (__NR_open, (long)argv[1], O_RDONLY);
+  if (0 > fd)
+    {
+      syscall3 (__NR_write, 2, (long)"Error opening file\n", 19);
+      syscall1 (__NR_exit, 1);
     }
 
-    char buffer[BUFFER_SIZE] = {0};
-    unsigned long int read_size = 0;
+  char buffer[BUFFER_SIZE] = { 0 };
+  unsigned long int read_size = 0;
 
-    // read file
-    while ((read_size = syscall3(__NR_read, fd, (long)buffer, BUFFER_SIZE)) > 0) {
-        // write file
-        syscall3(__NR_write, 1, (long)buffer, read_size);
-        clear_buff(buffer);
+  // read file
+  while ((read_size = syscall3 (__NR_read, fd, (long)buffer, BUFFER_SIZE)) > 0)
+    {
+      // write file
+      syscall3 (__NR_write, 1, (long)buffer, read_size);
+      clear_buff (buffer);
     }
 
-    // close file
-    syscall1(__NR_close, fd);
+  // close file
+  syscall1 (__NR_close, fd);
 }
