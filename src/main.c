7d4c24d4e251496d518437185c212500cbf7848c
notnotnull <jesteralex@hotmail.com>
2026-03-05 15:10:05 -0500

    add argc checking and a string length function

--- src/main.c
+++ src/main.c
@@ -8,6 +8,9 @@
 #define BUFFER_SIZE 128
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
+#define NULL ((void *)0)
+#define SUCCESS 0
+#define FAILURE -1
 
 /*
  * Startup routine
@@ -82,10 +85,7 @@ static inline __attribute__ ((always_inline)) long
 syscall3 (long n, long a1, long a2, long a3)
 {
   long ret;
-  asm ("syscall\n\t"
-       : "=a"(ret)
-       : "a"(n), "D"(a1), "S"(a2), "d"(a3)
-       : "memory", "rcx");
+  asm ("syscall\n\t" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2), "d"(a3) : "memory", "rcx");
   return ret;
 }
 
@@ -105,11 +105,31 @@ syscall4 (long n, long a1, long a2, long a3, long a4)
 {
   long ret;
   register long r10 __asm__ ("r10") = a4;
-  asm ("syscall\n\t"
-       : "=a"(ret)
-       : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10)
-       : "memory", "rcx");
+  asm ("syscall\n\t" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10) : "memory", "rcx");
   return ret;
+}
+
+/**
+ * @brief Calculates the length of a given string.
+ *
+ * @param str (const char *) The string for which the length is to be
+ * calculated.
+ * @return (unsigned long): Length of the string or 0 if the string is null.
+ */
+static inline __attribute__ ((always_inline)) unsigned long int
+len (const char *str)
+{
+  if (str == NULL)
+    {
+      return 0;
+    }
+
+  unsigned long length = 0;
+  while (str[length] != '\0')
+    {
+      length++;
+    }
+  return length;
 }
 
 /**
@@ -127,31 +147,50 @@ clear_buff (char *buffer)
     }
 }
 
+static inline __attribute__ ((always_inline)) int
+arg_check (int argc)
+{
+  const char *error_message = "Usage: syscat <file>\n\0";
+  if (argc < 2)
+    {
+      syscall3 (__NR_write, STDERR_FILENO, (long)(error_message), len (error_message));
+      return FAILURE;
+    }
+  return SUCCESS;
+}
+
 int
 main (int argc, char *argv[])
 {
-  (void)argc;
-  // open file
+  int status = 0;
   int fd = 0;
+  unsigned long int read_size = 0;
+  char buffer[BUFFER_SIZE] = { 0 };
+  const char *file_error_message = "Error opening file\n\0";
+
+  status = arg_check (argc);
+  if (status < 0)
+    {
+      return FAILURE;
+    }
+
+  // open file
   fd = syscall2 (__NR_open, (long)(argv[1]), O_RDONLY);
   if (fd < 0)
     {
-      syscall3 (__NR_write, STDERR_FILENO, (long)"Error opening file\n", 19);
+      syscall3 (__NR_write, STDERR_FILENO, (long)(file_error_message), len (file_error_message));
       syscall1 (__NR_exit, 1);
     }
 
-  char buffer[BUFFER_SIZE] = { 0 };
-  unsigned long int read_size = 0;
-
   // read file
-  while ((read_size = syscall3(__NR_read, fd, (long)buffer, BUFFER_SIZE)) > 0)
+  while ((read_size = syscall3 (__NR_read, fd, (long)buffer, BUFFER_SIZE)) > 0)
     {
       // write file
-      syscall3 (__NR_write, STDOUT_FILENO, (long)(buffer), read_size);
+      syscall3 (__NR_write, STDOUT_FILENO, (long)buffer, read_size);
       clear_buff (buffer);
     }
 
   // close file
   syscall1 (__NR_close, fd);
-  return 0;
+  return SUCCESS;
 }
